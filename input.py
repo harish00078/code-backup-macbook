c03dd0af24de7ff165f57d0728cbefc8e1a636cc
harishkunar0001 <103200405+harishkunar0001@users.noreply.github.com>
2022-04-07 20:13:17 +0530

    update

--- input.py
+++ input.py
@@ -1,6 +1,7 @@
 from xml.dom.minidom import NamedNodeMap
 
 name = input("enter your name ")
+major = input("enter your major")
 age = input("enter your age ")
 
-print("hello" +name+ "you are "+age)
+print("hello" +name+ "you are "+age+"tell me your major"+major)
